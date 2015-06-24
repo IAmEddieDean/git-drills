2b2689c0f7a41b6333a1f91374d0ca5b2a754d8b
bonnieso <so_bonnie@hotmail.com>
2015-06-23 20:36:01 -0700

    resolving conflicts

--- sum.js
+++
@@ -1,5 +0,0 @@
-var sum = function(num1, num2){
-  return num1 + num2;
-};
-
-sum(10+6);
--- vowels.js
+++ vowels.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-var vowels = function(something){
-  var result = something.replace(/[aeiouAEIOU]/g, "");
-  console.log(result);
-  return result;
-};
 
-vowels("Someone help me! I need help! Oh man buddy!")
-=======
 function removeVowels(str) {
   return str.replace(/[aeiou]/gi, "");
 }
 
 console.log(removeVowels("HelloOOOO") === "Hll");
->>>>>>> 9d901c388037b7607b06a683cb0b5976bdaf9685
